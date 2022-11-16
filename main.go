d50f0a1a73bd387b8d65b49311ac43d2a66500b5
Eli Richardson <elijahrich02@icloud.com>
2022-11-16 07:20:55 -0600

    cli added

--- main.go
+++ main.go
@@ -2,21 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/eli-rich/glock/src/encryption"
+	"github.com/eli-rich/glock/src/files"
 	"github.com/eli-rich/glock/src/key"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	sha := encryption.Shasum("test")
-	fmt.Printf("%x\n", sha)
-	key.SetKey(fmt.Sprintf("%x", sha))
-	key.GetKey()
-	f, _ := os.Open("test.txt")
-	defer f.Close()
-	encryption.EncryptFile(key.GetKey(), f)
-	r, _ := os.Open("test.txt.enc")
-	defer r.Close()
-	encryption.DecryptFile(key.GetKey(), r)
+	app := &cli.App{
+		Name:  "glock",
+		Usage: "Encrypt and decrypt files",
+		Commands: []*cli.Command{
+			{
+				Name:    "key",
+				Aliases: []string{"k"},
+				Usage:   "Set your encryption key",
+				Action: func(c *cli.Context) error {
+					k := encryption.Shasum(c.Args().Get(0))
+					key.SetKey(fmt.Sprintf("%x", k))
+					fmt.Println("Key saved to ~/.config/glock/glock.key")
+					return nil
+				},
+			},
+			{
+				Name:    "encrypt",
+				Aliases: []string{"e"},
+				Usage:   "Encrypt a file",
+				Action: func(c *cli.Context) error {
+					files.Encrypt(c.Args().Get(0))
+					return nil
+				},
+			},
+			{
+				Name:    "decrypt",
+				Aliases: []string{"d"},
+				Usage:   "Decrypt a file",
+				Action: func(c *cli.Context) error {
+					files.Decrypt(c.Args().Get(0))
+					return nil
+				},
+			},
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
